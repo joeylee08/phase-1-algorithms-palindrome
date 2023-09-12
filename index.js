535230922d021d4243b86498429ba313380b47ee
Joseph <josephylee72@gmail.com>
2023-09-11 23:36:07 -0700

    completed palindrome lab

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word === word.split("").reverse().join("");
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
